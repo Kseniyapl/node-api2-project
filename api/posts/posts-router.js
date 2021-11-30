8b939a5080a3664ef3334da17cb32e6a856932ed
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-11-30 18:48:12 -0500

    Add get all posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,22 @@
-// implement your posts router here
+const express = require('express')
+const router = express.Router()
+const Post = require('./posts-model')
+
+router.get('', (req, res) => {
+    Post.find()
+    .then(posts=>{
+        res.status(200).json(posts)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({
+            message: 'Error retrieving the posts'
+        })
+    })
+})
+
+router.get('/:id', (req, res) => {
+    
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -2,17 +2,13 @@
 // require your posts router and connect it here
 
 const express = require('express');
-
+const postsRouter = require('./posts/posts-router')
 const server = express();
 
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.send(`
-      <h2>Shelter API</h>
-      <p>Welcome to the Shelter API</p>
-    `);
-  });
-  
+server.use('/api/posts', postsRouter);
+
+
   module.exports = server;
   
