b53c2c9a3ccb8e660318ae59b7efe5c5f88e32bd
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-11-30 19:14:00 -0500

    Add get post by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -10,13 +10,26 @@ router.get('', (req, res) => {
     .catch(err=>{
         console.log(err)
         res.status(500).json({
-            message: 'Error retrieving the posts'
+            message: 'Error retrieving posts'
         })
     })
 })
 
 router.get('/:id', (req, res) => {
-    
+    Post.findById(req.params.id)
+    .then(post => {
+        if (post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({message: "The post with the specified ID does not exist"})
+        }
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(500).json({
+            message: 'Error retrieving the post'
+        })
+    })
 })
 
 module.exports = router
